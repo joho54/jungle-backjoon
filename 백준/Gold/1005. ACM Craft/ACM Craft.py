a3f223d1f0bfe2383694d6479d3826076452ebd3
조현호_ChoHyeonHo <70460688+joho54@users.noreply.github.com>
2025-08-22 21:06:22 +0900

    [Gold III] Title: ACM Craft, Time: 1312 ms, Memory: 45600 KB -BaekjoonHub

---
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -0,0 +1,65 @@
+import sys
+from collections import defaultdict, deque
+
+def solve(times: list, graph: defaultdict, w: int):
+    ordered, sources = topo_sort(graph=graph)
+    distances = {v:0 for v in graph}
+    for source in sources:
+        distances[source] = times[source]
+    
+    for u in ordered:
+        for v in graph[u]:
+            new_d = distances[u] + times[v]
+            if new_d > distances[v]:
+                distances[v] = new_d
+    return distances[w]                
+
+def topo_sort(graph: defaultdict):
+    indegree = {v:0 for v in graph}
+    queue = deque()
+    
+    # set indegree
+    for u in graph:
+        for v in graph[u]:
+            indegree[v] += 1
+    sources = []
+    # set start points
+    for u in indegree:
+        if indegree[u] == 0:
+            queue.append(u)
+            sources.append(u)
+    
+    # start loop
+    ordered = []
+    while queue:
+        # pop
+        u = queue.pop()
+        ordered.append(u)
+        for v in graph[u]:
+            indegree[v] -= 1
+            if indegree[v] == 0:
+                queue.append(v)
+    return ordered, sources
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    t = int(input().strip())
+    for _ in range(t):
+        n, k = tuple(map(int, input().split()))
+        times = tuple(map(int, input().split()))
+        times = {i+1:times[i] for i in range(len(times))}
+        
+        arr = [
+            tuple(map(int, input().split()))
+            for _ in range(k)
+        ]
+        graph = defaultdict(list)
+        for u in range(1, n+1):
+            graph[u] = []
+        
+        for u, v in arr:
+            graph[u].append(v)
+            
+        w = int(input().strip())
+        
+        print(solve(times, graph, w))
